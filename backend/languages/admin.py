8812bcabead0e736ea87f5f8ca33c3cc03edf479
jonat <jshall@bu.edu>
2020-10-24 21:54:02 -0400

    further initialization of django setup

--- backend/languages/admin.py
+++ backend/languages/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Language
+from .models import Speaker
+
+
+
+admin.site.register(Language)
+admin.site.register(Speaker)
