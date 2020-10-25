d8c4ae95347df4a9ba4259ff3bfd5702fed2a4a6
jonat <jshall@bu.edu>
2020-10-25 14:13:05 -0400

    deleted unnecessary subproject (accounts)

--- backend/accounts/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Speaker(models.Model):
-    name = models.CharField(max_length=200, null=True)
-    email = models.CharField(max_length=200, null=True)
-    phone = models.CharField(max_length=200, null=True)
