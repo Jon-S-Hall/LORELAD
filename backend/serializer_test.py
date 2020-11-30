3394546be396d9016668d3a640fb13c3825153ab
jonat <jshall@bu.edu>
2020-11-29 23:59:10 -0500

    made some changes to backend

--- backend/serializer_test.py
+++
@@ -1,7 +0,0 @@
-from master.models import Record
-from loreladAPI.serializers import RecordSerializer
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
-from master.views import createRecord
-
-record = Record(title = )
